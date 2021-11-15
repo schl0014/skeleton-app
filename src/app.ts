96237ca23e6d7c1296cff099a30e78256b1f91c0
schl0014 <schl0014@hz.nl>
2021-11-15 10:49:31 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,20 +5,38 @@ class Animal {
 
   private readonly sound: string;
 
+  /**
+   *
+   * @param name name
+   * @param legs legs
+   * @param sound sound
+   */
   public constructor(name: string, legs: number, sound: string) {
     this.name = name;
     this.legs = legs;
     this.sound = sound;
   }
 
+  /**
+   *
+   * @returns name
+   */
   public getName(): string {
     return this.name;
   }
 
+  /**
+   *
+   * @returns legs
+   */
   public getLegs(): number {
     return this.legs;
   }
 
+  /**
+   *
+   * @returns sound
+   */
   public getSound(): string {
     return this.sound;
   }
